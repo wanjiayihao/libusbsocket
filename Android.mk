6b8327100a44bc57bfa077047cbb218c5964cc77
zhuhuajian <zhuzhuajian@lianluo.com>
2017-11-29 09:47:42 +0800

    add files

---
+++ Android.mk
@@ -0,0 +1,71 @@
+LOCAL_PATH:= $(call my-dir)
+
+# libusbdemon
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+
+
+
+#for plist 
+LOCAL_SRC_FILES += \
+                   src/libplist/Array.cpp \
+                   src/libplist/base64.c \
+                   src/libplist/Boolean.cpp \
+                   src/libplist/bplist.c \
+                   src/libplist/bytearray.c \
+                   src/libplist/Data.cpp \
+                   src/libplist/Date.cpp \
+                   src/libplist/Dictionary.cpp \
+                   src/libplist/hashtable.c \
+                   src/libplist/Integer.cpp \
+                   src/libplist/Key.cpp \
+                   src/libplist/Node.cpp \
+                   src/libplist/plist.c \
+                   src/libplist/ptrarray.c \
+                   src/libplist/Real.cpp \
+                   src/libplist/String.cpp \
+                   src/libplist/Structure.cpp \
+                   src/libplist/time64.c \
+                   src/libplist/Uid.cpp \
+                   src/libplist/xplist.c \
+                   src/libplist/cnary.c \
+                   src/libplist/iterator.c \
+                   src/libplist/list.c \
+                   src/libplist/node.c \
+                   src/libplist/node_iterator.c \
+                   src/libplist/node_list.c 
+
+LOCAL_C_INCLUDES += \
+  $(LOCAL_PATH)/include \
+  $(LOCAL_PATH)/include/plist \
+  $(LOCAL_PATH)/src/libplist \
+
+
+
+LOCAL_SRC_FILES += \
+  src/collection.c \
+  src/socket.c \
+  src/libusbmuxd.c \
+  src/iproxy.c
+
+
+
+include external/stlport/libstlport.mk
+
+
+
+LOCAL_SHARED_LIBRARIES += \
+	libcutils \
+	libutils \
+
+
+LOCAL_LDLIBS := -llog
+
+LOCAL_CFLAGS += -pie -fPIE
+
+LOCAL_MODULE := usbsocket
+
+#include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
